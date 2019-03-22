0d1e25ce34db8e4befd6d67328d90d1e7750dcc6
Matheus <silva.matheusdorneles@gmail.com>
2019-03-22 11:21:28 -0700

    deleted unnecessary files

--- includes/addproduct.js
+++ includes/addproduct.js
@@ -11,5 +11,29 @@ $(document).ready(function () {
 		$("#products_container").fadeIn();
 		
 	});	
+
+	//Results (initially hidden)
+	/*
+	$("#topResults").hide();
+	$("#results").hide();
+	$("#score").hide();
 	
+	//if they forgot to fill in a question, their score will not show
+	if (count == 0) {
+		$("#topResults").html("You scored " + score + " out of 5");
+		$("#results").html("<h3 class='redWithYellow'>RESULTS FOR " + name + ": You scored " + score + " out of 5</h3>");
+		$("#topResults").fadeIn(3000);
+		$("#results").fadeIn(3000);
+		
+		//flashing  'PERFECT' message
+		if (score == 5) {
+			$("#score").html("<h3 class='redWithYellow'>You scored 5/5. Perfect!</h3>");
+			for (var i = 0; i < 10; i++) {
+				$("#score").fadeIn(500);
+				$("#score").fadeOut(500);
+			}
+			$("#score").fadeIn(500);
+		}
+	}
+	*/
 });
--- includes/head.php
+++ includes/head.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 <link rel="stylesheet" href="includes/head.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="includes/cat.css" type="text/css" media="screen" />
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
