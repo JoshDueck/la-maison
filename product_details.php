cb8d47e06e4d3b29a6ed882f4d0b85afabe1cc52
Matheus <silva.matheusdorneles@gmail.com>
2019-03-28 16:06:00 -0700

    created product_details.php to display detailed info of products

--- product_details.php
+++ product_details.php
@@ -1,15 +1,31 @@
+<?php
+session_start();
+$_SESSION['prod_details'] = "product_details variable";
+
+
+
+
+?>
 <html>
 <link rel="stylesheet" href="includes/product_details.css" type="text/css" media="screen" />
 <head>
 </head>
 <body>
 <?php
-session_start();
 
 
 include("includes/head.php"); // inserts header at the top
 
 echo "<article>";
+
+if (isset($_SESSION['customer_id'])) {
+	//echo "You are logged in";
+} else {
+	//echo "NOT LOGGED IN";
+	if ($_GET['quantity']>0){
+		echo "<p id=\"failure_message\"> You cannot add products to the cart without being logged in. <a href=\"login.php\">Click here to login</a></p>";
+	}
+}
 
 include("mysqli_connect.php"); // connection string is $dbc	
 
@@ -17,16 +33,24 @@ include("mysqli_connect.php"); // connection string is $dbc
 $quantity = $_GET['quantity'];
 $product_id = $_GET['product_id'];
 
+
+
 // checks if the customer added anything to the cart
-if ($quantity != "" && $quantity != 0){
+if (($quantity != "") && ($quantity != 0) && (isset($_SESSION['customer_id']))){
 // added, display success message and add to cart
-	//  success message
-	echo "<p id='success_message'> You have succefully added ".$quantity." products. <a href='http://deepblue.cs.camosun.bc.ca/~ics19901/shopping_cart.php'>View Shopping cart</a>";
 	// update product in database
-	query = "INSERT INTO CART (CUSTOMER_customer_id, PRODUCT_product_id, quantity) VALUES (1,".$product.",".$quantity.") ON DUPLICATE KEY UPDATE quantity=".$quantity.";";
+	$insert = "INSERT INTO CART (CUSTOMER_customer_id, PRODUCT_product_id, quantity) VALUES (".$_SESSION['customer_id'].", $product_id, $quantity) ON DUPLICATE KEY UPDATE quantity= quantity + '$quantity';";
 	
-} else{
-	echo "No quantity";
+	if (mysqli_query($dbc, $insert)) { // runs when successfully inserted
+		//  success message
+		echo "<p id=\"success_message\"> You have successfully added $quantity products. <a href='http://deepblue.cs.camosun.bc.ca/~ics19901/shopping_cart.php'>View Shopping cart</a></p>";
+				//You now have X products in your shopping cart. Subtotal: XXX
+	} else { // failed to insert
+		echo "Error: ".mysqli_error($dbc);
+	}
+	
+} else{ // no quantity or not logged in
+	//echo "No quantity"; // here for debugging purposes
 }
 
 
@@ -43,38 +67,39 @@ $result = mysqli_query($dbc, $query);
 	}
 	else { // product was found in the database
 	
-		while($prod_row=mysqli_fetch_array($result, MYSQLI_ASSOC)){
+		$prod_row=mysqli_fetch_array($result, MYSQLI_ASSOC);
+			// display product info
 			echo "
-				<div id=\"img_container\">
-			<img src=\"".$prod_row['product_image']."\" />
-		</div> <!-- end of img_container -->
-		<div id=\"details_container\">
+			<div id=\"img_container\">
+				<img src=\"".$prod_row['product_image']."\" />
+			</div> <!-- end of img_container -->
+			<div id=\"details_container\">
 		
-		<div id=\"name_container\">
+			<div id=\"name_container\">
 		
-			<h2>".$prod_row['product_name']."</h2>
+				<h2>".$prod_row['product_name']."</h2>
 		
-		</div> <!-- end of name_container -->
+			</div> <!-- end of name_container -->
 		
-		<div id=\"price_container\">
+			<div id=\"price_container\">
 		
-			<h3>$".$prod_row['product_price']."</h3>
+				<h3>$".$prod_row['product_price']."</h3>
 		
-		</div> <!-- end of price_container -->
+			</div> <!-- end of price_container -->
 		
-		<div id=\"buttons_container\">
+			<div id=\"buttons_container\">
 			
 			";
 			
-			if (isset($_SESSION['username'])) {
-				echo "You are logged in";
+			if (isset($_SESSION['customer_id'])) {
+				//echo "You are logged in"; // debugging purposes
 				// if user is logged in, add stuff to the cart
 				echo "<form method=\"GET\" action=\"product_details.php?product_id=".$prod_row['product_id']."&quantity=".quantity."\" enctype=\"multipart/form-data\">";
 				
 			} else{
-				echo "you are not logged in";
+				//echo "you are not logged in";
 				// if user is NOT logged in, redirect them to login page
-				echo "<form action=\"login.php\" class=\"modal-content\" enctype=\"multipart/form-data\">";
+				echo "<form method=\"GET\" action=\"product_details.php?product_id=".$prod_row['product_id']."&quantity=".quantity."\" enctype=\"multipart/form-data\">";
 			}
 			
 			echo "
@@ -97,12 +122,9 @@ $result = mysqli_query($dbc, $query);
 			$proddesc = str_replace("\"\"","\"",$proddesc);
 			echo str_replace("\\t-","&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp• ",$proddesc);
 			echo "</p></div> <!-- end of description_container -->
-	
 			";
-		}
-}
-
-
+		
+	}
 ?>
 	
 </article>
