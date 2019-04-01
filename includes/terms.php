641ad892f4b37c8088ae3170c309527bcf08a5a2
C0476435 <alinrosemanjila@gmail.com>
2019-04-01 09:14:18 -0700

    Terms.php committed

---
+++ includes/terms.php
@@ -0,0 +1,9 @@
+<?php
+echo "<H1><pre>Terms and Conditions</pre></H1>
+<br>
+<h3> Account: </h3>
+<br>
+<br>
+<h2> You need your La Maison account to use certain service s and you may be required to be logged in to the account qnd have a valid pament associated with it. The responsibility of managing the confidentiality of your account and password and restrictions to your account. The activities done under your account is your responsibility.
+"
+?>
