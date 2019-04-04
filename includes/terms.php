b3403486b28be486d711b5985f0e67b512aebad2
Jane <yoonji1202@gmail.com>
2019-04-04 13:32:51 -0700

    privacy policy page

---
+++ includes/terms.php
@@ -0,0 +1,55 @@
+<?php
+echo "<H1>Terms and Conditions</H1>
+
+<p><b>Last updated: April 03, 2019</b></p>
+<br>
+
+<h1>Thanks for visiting La Maison Web Site</h1>
+<br>
+
+<h1>Terms of Use</h1>
+<br>
+
+<h2>Agreement with our Temrs and Conditions</h2>
+
+<p>By using the site, you agree to accept the terms and conditions of this agreement(\"terms and conditions\"), so please be sure to rea them carefully. If you do not agree with our terms and conditions, please do not use our site. We reserve the right, at our discretion, to change, modify, add or remove portions of theses terms and conditions at any time. Unless the law requires otherwise, your continued use of our site following the posting of changes to these terms will mean that you accept those changes.</p>
+<br>
+<h2>User Information</h2>
+<p>In the course of using the site, you may provide or we may collect personal information about you. We are committed to protecting the privacy of our customers' personal information and explain our policies with respect to the use and disclosure of such personal information in our privacy policy incorporated into these terms and conditions.</p>
+<p> You can create a user account on the site that allows you to access and user certain of its functions and features. You acknowledge and agree that the user identification chosen by you for access and utilization of the site(\"Email\"), and the password selected by and used by you in conjunction with the respective Email are to be kept secure, secret and confidential(\"Password\"). We are authorized to accept such Email and Password as conclusive evidence that you have accessed or utilized the Site.</p>
+<br>
+
+<h1>Privacy Policy</h1>
+
+<p>At La Maison, we are committed to protecting the privacy of our customers and therefore do not sell or rent our customers' Personal Information to any third party. For the purpose of this Privacy Policy. We do not share your Personal Information with any third parties for their direct marketing purposes without first providing you with the opportunity to opt-in or opt-out of such sharing. You may request information about how we manage your Personal Information consistent with this Privacy Policy by contacting us at <a href = \"mailto:lamaison.homeinterior@gmail.com\"> La Maison</a>.</p>
+<br>
+<br>
+<h2>What personal data we collect and process</h2>
+
+<p> La Maison collects several different types of personal data for various purposes. Personal may include, but is not limited to:
+
+<ul>
+	<li> Email address </li>
+	<li> First name and last name </li>
+	<li> Address, City, Province, Postal code, Country </li>
+	<li> Last login time</li>
+	<li> Privacy terms accepted </li>
+</ul> </p>
+<br>
+
+<h3><b>Personal Information</b></h3>
+<p>In order to process orders efficiently, when you place an order with us or register an account on the Site we actively collect the following personal information: name, email, shipping address and information that may be of a personally identifiable nature. </p>
+<br>
+
+<h3><b>Site Usage Information</b></h3>
+<p>We may inactively collect from you information that is not in the same category as Personal Information. For example, we might track information about the date and time you access the Site, the type of web browser you used, and the web site from which you connect to our Site.</p>
+<br>
+
+<h3><b>Security of the Information you provicde</b></h3>
+<p>You may be able to create an account on our Site with an email and password, including if you set up a customer account. To help protect your information, we have required password standards. We recommend you exercise extreme care in protecting your password information and do not publicly share any of your passwords. You are responsible for maintaining the strict confidentiality of your account password, and you are responsible for any access to or use of the Site by you or another person or entity using your password, whether or not such access or use has been authorized by or on behalf of you.</p>
+<br>
+
+<h2> How do we use the personal data that you provide and we collect?</h2>
+
+<p> We collect Personal Information for the purposes of fulfilling orders processing returns and exchanges and providing you with the services you have chosen, providing you with information we believe may be of interest including mailing order information. We do not sell Personal Information and we do not disclose Personal Information to any other party, except as set forth in this Privacy Policy or as required by applicable law or regulatory requirements. Unless otherwise provided in this Privacy Policy, any non-Personal Information you transmit to the Site or we collect from the Site will be treated as non-confidential and non-proprietary and you agree that we may use any such non-Personal Information in any manner, for any purpose, without credit or compensation to you.</p>"
+?>
