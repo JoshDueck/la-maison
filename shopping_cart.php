9dfa5951df32770292b5c50fe1a71e1921385dff
matheusDS <silva.matheusdorneles@gmail.com>
2019-04-03 19:50:33 -0700

    fixed options at the bottom of the shopping_cart page

--- shopping_cart.php
+++ shopping_cart.php
@@ -94,18 +94,18 @@ if(isset($_SESSION['customer_id']) && ($prod_row >0)){
 	echo "<table cellpadding='5' cellspacing='5' style='width:100%;'>";
 	echo "<tr>";
 
-	echo "<td class=\"image_row\">Product Image</td>";
-	echo "<td class=\"name_row\" style='width: 150px; word-break: break-all;'>Product Name</td>";
-	echo "<td>Price</td>";
-	echo "<td>Quantity</td>";
-	echo "<td>Subtotal</td>";
-	echo "<td>
+	echo "<th class=\"image_row\">Product Image</th>";
+	echo "<th class=\"name_row\">Product Name</th>";
+	echo "<th>Price</th>";
+	echo "<th>Quantity</th>";
+	echo "<th>Subtotal</th>";
+	echo "<th>
 		<form method=\"POST\" action=\"shopping_cart.php\" enctype=\"multipart/form-data\">
 		<input type=\"hidden\" id=\"delete_all\" name=\"delete_all\" value=\"true\">
 
 	    <button type=\"submit\" name=\"delete\" id=\"delete\">Remove All</button>
 		</form>
-	</td>";
+	</th>";
 
 	echo "</tr>";
 	$total=0;
@@ -129,10 +129,10 @@ if(isset($_SESSION['customer_id']) && ($prod_row >0)){
 		$total += $subtotal;
 		
 		echo "<tr>";
-		echo "<td class=\"image_row\" align='center' style=\"\"><img src=\"".$prod_row['product_image']."\" style=\"width:150px;\"></td>";
-		echo "<td class=\"name_row\" align='center' style=' word-wrap:break-word; word-break: break-all;'>$prod_name</td>";
-		echo "<td align='center'>$".$prod_row['product_price']."</td>";
-		echo "<td align='center'>
+		echo "<td class=\"image_row\"><a href=\"product_details.php?product_id={$prod_row['product_id']}\"><img class=\"prod_imgs\" src=\"".$prod_row['product_image']."\"></a></td>";
+		echo "<td class=\"name_row\">$prod_name</td>";
+		echo "<td>$".$prod_row['product_price']."</td>";
+		echo "<td>
 		<input type=\"button\" class=\"decrement_btn\" id='decrement_btn".$rownum."' value=\"-\" />
 		<input type=\"number\" class=\"quantity\" id=\"quantity".$rownum."\" name=\"quantity".$rownum."\" value=\"{$prod_row['quantity']}\" />
 		<input type=\"button\" class=\"increment_btn\" id='increment_btn".$rownum."' value=\"+\" />
@@ -142,36 +142,38 @@ if(isset($_SESSION['customer_id']) && ($prod_row >0)){
 
 		echo "<td class=\"subtotal\" id=\"subtotal".$rownum."\">= \$$subtotal";		
 		// remove from cart using GET
-		echo "<td style='width:100px;'><a href='shopping_cart.php?product_id={$prod_row['product_id']}' style='text-decoration:none;'>Remove</a></td>";
+		echo "<td><a href='shopping_cart.php?product_id={$prod_row['product_id']}' style='text-decoration:none;'>Remove</a></td>";
 
 		echo "</tr>";
 		$rownum += 1;
 	}
-	echo "<tr style='color:#ff0000;'>";
-	echo "<td></td>";
-	echo "<td></td>";
-	echo "<td></td>";
-	echo "<td></td>";
-	echo "<td align='center'>Total</td>";
-	echo "</tr>";
-	echo "<tr style='color:#000000;'>";
-	echo "<td></td>";
-	echo "<td></td>";
-	echo "<td></td>";
-	echo "<input type=\"hidden\" id=\"save_changes\" name=\"save_changes\" value=\"true\">";
-	echo "<td><button type='submit' id=\"save_changes_btn\">Save changes</button></td>";
-			echo "</form>";
-
-	echo "<td><br /><b>= \$$total</b></td>";
-	echo "<td><br /><a href='check_out.php'><img src = 'images/checkout.png' width='120px' height='40px'></a></td>";
-
-	echo "</tr>";
-	echo "</table>";			
+	echo "</table>";
+	
+	
+	// start of floating_total div
+	echo "<div id='floating_total'>
+		
+			<a href='check_out.php'><img src = 'images/checkout.png' width='120px' height='40px'></a>
+			
+			<p>Total: = \$$total</p>
+			
+			<input type=\"hidden\" id=\"save_changes\" name=\"save_changes\" value=\"true\">
+			<button type='submit' id=\"save_changes_btn\">Save changes</button>
+			</form>
+			
+	</div>
+	"; // end of floating_total div
 	
 }else{ // shopping cart is empty
 
-	echo "<font color='#000000'><h1 align='center'>Your shopping cart is empty.<br /><br><a href='index.php'><input type='button' class='link1' value='Continue shopping' style='width:150px; height:35px; font-size:15px;'></a></h1></font>";
+	echo "<h1 align='center'>Your shopping cart is empty.<br><br><a href='index.php'><input type='button' class='link1' value='Continue shopping'></a></h1>";
 }
+
+
+include("includes/footer.html");
+
+echo "<div id='bottom_space'></div>";
+
 ?>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
