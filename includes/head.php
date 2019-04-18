06d2c5ff5803fbe53fe4e065df5ec5c4ec7bc521
C0476435 <alinrosemanjila@gmail.com>
2019-04-18 12:14:07 -0700

    Styling of pages changed

--- includes/head.php
+++ includes/head.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html>
 <link rel="stylesheet" href="includes/head.css" type="text/css" media="screen" />
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" />
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
-<body>
+<body >
 <?php
 session_start();
 ?>
-<div class="header">
+<div class="header" background="headbg.jpeg" >
 <div >
-  <a href="index.php" class="logo"><h3 id="logo">La Maison</h3></a>
+  <a href="index.php" class="logo"><h3 id="logo">La M A I S O N </h3><i id="homeicon" style="display:inline-block" class="fas fa-home"></i></a>
   <h3 class="right-top">
 	<?php 
 	if (isset($_SESSION['customer_id'])) { 
@@ -26,7 +28,7 @@ session_start();
 	<br>
 	<br>
   <div class="searchbar">
-  
+  <!--<input class="topnav" type="text" id="top" placeholder="Search here"><img id="search_pic" src="search.jpg"> -->
 <?php
 	if (isset($_SESSION['customer_id'])) { // checking if user is logged in
 ?>
@@ -83,6 +85,7 @@ echo "<div class='navbar'>"; // start navbar
 
 while($cat_row=mysqli_fetch_array($cat_rows, MYSQLI_ASSOC)){
 	// display each cat
+	echo "<li>";
 	echo "<div class='subnav'>";
 	echo "<a class='subnavbtn' href='http://deepblue.cs.camosun.bc.ca/~ics19901/products_display.php?category_name={$cat_row['category_name']}'>".$cat_row['category_name']."<i class='fa fa-caret-down'></i></a><br><br>";
 	echo "<div class='subnav-content' id='desired_element'>";
@@ -99,6 +102,7 @@ while($cat_row=mysqli_fetch_array($cat_rows, MYSQLI_ASSOC)){
 
 	echo "</div>"; // end of subnav-content div
 	echo "</div>"; // end of subnav div
+	echo "</li>";
 }
 
 
